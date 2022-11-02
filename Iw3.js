6c77db326b735fceb8ee7790034789c0da66a866
vikrant10 <115461273+vikrants10@users.noreply.github.com>
2022-11-02 09:15:48 +0530

    Update Iw3.js

--- Iw3.js
+++ Iw3.js
@@ -1,4 +1,4 @@
-// Given any character, if it is a vowel print "Vowel"
+// Given any character, if it is a vowel print "Consonent"
 
 let char = "s";
 if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
